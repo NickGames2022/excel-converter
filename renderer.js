ec041cccd7739e26780347640efa2226767cd6d6
NickGames2022 <38009918+NickGames2022@users.noreply.github.com>
2024-06-04 01:14:31 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -7,13 +7,34 @@ document.getElementById('excelFileInput').addEventListener('change', (event) => 
   const file = event.target.files[0];
   if (!file) return;
 
+  const fileNameDisplay = document.getElementById('fileNameDisplay');
+  const fileName = document.getElementById('fileName');
+  fileName.textContent = file.name;
+  fileNameDisplay.style.display = 'block';
+
   const reader = new FileReader();
   reader.onload = (e) => {
     const data = new Uint8Array(e.target.result);
     const workbook = XLSX.read(data, { type: 'array' });
-    const sheetName = workbook.SheetNames[0];
+    const sheetName = 'Series Database';
     const sheet = workbook.Sheets[sheetName];
     jsonData = XLSX.utils.sheet_to_json(sheet);
+
+
+    jsonData.forEach(row => {
+      if (row['__EMPTY_1']) {
+        row['Series'] = row['__EMPTY_1'];
+        delete row['__EMPTY_1'];
+      }
+      if (row['__EMPTY_2']) {
+        row['Character'] = row['__EMPTY_2'];
+        delete row['__EMPTY_2'];
+      }
+      if (row['__EMPTY_3']) {
+        row['Points'] = row['__EMPTY_3'];
+        delete row['__EMPTY_3'];
+      }
+    });
 
     document.getElementById('convertButton').style.display = 'inline-block';
   };
@@ -40,15 +61,21 @@ document.getElementById('convertButton').addEventListener('click', () => {
     frameIndex = (frameIndex + 1) % animationFrames.length;
   }, 500);
 
+  
   setTimeout(() => {
     clearInterval(animationInterval);
     convertButton.innerText = 'Download JSON';
     convertButton.disabled = false;
     convertButton.id = 'downloadButton';
+    convertButton.classList.remove('green-button');
+    convertButton.classList.add('blue-button');
     convertButton.removeEventListener('click', convertButtonClickHandler);
     convertButton.addEventListener('click', downloadButtonClickHandler);
   }, 2000);
 });
+
+function convertButtonClickHandler() {
+}
 
 function downloadButtonClickHandler() {
   if (!jsonData) {
